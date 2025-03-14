cf6c206135add279b0116cf30f0d949233404a8b
Ralph Gabriel Mariano <gabrielmarianoofficial@gmail.com>
2025-03-14 11:29:19 +0800

    add trigger to workflow

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -26,7 +26,7 @@ export const sendEmail = async ({
       provider: resend({ token: config.env.resendToken }),
     },
     body: {
-      from: "JS Mastery <contact@adrianjsmastery.com>",
+      from: "Arcane Web apps <client.relations@arcanewebapps.com>",
       to: [email],
       subject,
       html: message,
